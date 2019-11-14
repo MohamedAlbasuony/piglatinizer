8898616c149c0a2c7502e8aba3bba7ab83d513ed
MohamedAlbasuony <malbasuony@mihsnyc.org>
2019-11-14 23:31:00 +0000

    Final Result PIG LATIN

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+   $("button").click(function(){
+       let finalResult = "";
+       let anything= $(".input").val();
+        let arr = anything.split(" ");  
+        arr.forEach(function(word){
+            let firstchar= word.charAt(0);
+            let result= word.substr(1);
+            result= result + firstchar + 'ay';
+            finalResult = finalResult + " " + result;
+
+        });
+
+
+      
 
 
 
-  
-  
-  
-  
+       $(".output").text(finalResult);
+});
 });
